6539ebea55ddff60f79b2e3147f8a7e3bc78f9fe
gjohnston2 <gjohnston2@gmail.com>
2025-09-06 01:29:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions 
+import col
 import requests
 import pandas
 # Write directly to the app
